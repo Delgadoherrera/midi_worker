6ac25755939e9aeb1a3bf2e1e66867d12038904c
Luciano Delgado <delgado.luciano.herrera@gmail.com>
2024-02-28 18:48:04 -0300

    notes_range

--- src/app.py
+++ src/app.py
@@ -1,21 +1,18 @@
 from flask import Flask, request, jsonify
 from werkzeug.utils import secure_filename
-from music21 import converter, stream, note, chord
+from music21 import converter, stream, note, chord, pitch
 import os
 
 app = Flask(__name__)
 
-# Directorio de archivos MIDI procesados
+# Configuraciones y directorios
+app.config['UPLOAD_FOLDER'] = './uploads'
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16 MB max size
 processed_dir = './assets/processed_midiFiles'
-# Subdirectorios para notas y acordes
 notes_dir = os.path.join(processed_dir, 'notes')
 chords_dir = os.path.join(processed_dir, 'chords')
 
-# Configuración para la carga de archivos
-app.config['UPLOAD_FOLDER'] = './uploads'
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16 MB max size
-
-# Asegurarse de que los directorios existen
+# Asegurar existencia de directorios
 for directory in [app.config['UPLOAD_FOLDER'], processed_dir, notes_dir, chords_dir]:
     if not os.path.exists(directory):
         os.makedirs(directory)
@@ -27,31 +24,46 @@ def process_midi():
         return jsonify({'error': 'No midi_files part'}), 400
 
     midi_files = request.files.getlist('midi_files')
-
-    # Validación simple para el número de archivos
     if len(midi_files) > 3:
         return jsonify({'error': 'Too many files, maximum is 3'}), 400
 
-    # Obtener otros parámetros
+    # Obtener parámetros
     x_bar = request.form.get('x_bar', default=8, type=int)
     key_name = request.form.get('key_name', default=False, type=lambda v: v.lower() == 'true')
     notes_export = request.form.get('notes', default=True, type=lambda v: v.lower() == 'true')
-    chord_export = request.form.get('chords', default=True, type=lambda v: v.lower() == 'true')
+    chord_export = request.form.get('chord', default=True, type=lambda v: v.lower() == 'true')
+    notes_range = request.form.get('notes_range', default="C1,C6")
 
-    # Procesar cada archivo MIDI
+    # Procesar archivos
     for file in midi_files:
         if file and file.filename.endswith('.mid'):
             filename = secure_filename(file.filename)
             file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
             file.save(file_path)
-            # Llamada a la función modificada para procesar el archivo
-            process_single_midi_file(file_path, x_bar, key_name, notes_export, chord_export)
+            process_single_midi_file(file_path, x_bar, key_name, notes_export, chord_export, notes_range)
 
     return jsonify({'message': 'Files processed successfully'}), 200
 
-# Función para procesar un único archivo MIDI
-def process_single_midi_file(midi_path, desired_duration, export_key_name, export_notes, export_chords):
-    midi_file = converter.parse(midi_path) 
+def adjust_note_octave(note_obj, lower_bound, upper_bound):
+    while note_obj.pitch < lower_bound or note_obj.pitch > upper_bound:
+        if note_obj.pitch < lower_bound:
+            note_obj.octave += 1
+        elif note_obj.pitch > upper_bound:
+            note_obj.octave -= 1
+    return note_obj
+
+
+
+def process_single_midi_file(midi_path, desired_duration, export_key_name, export_notes, export_chords, notes_range):
+    lower_bound_str, upper_bound_str = notes_range.split(',')
+    
+    # Incrementar la octava especificada por parámetro
+    lower_bound = pitch.Pitch(lower_bound_str)
+    upper_bound = pitch.Pitch(upper_bound_str)
+    lower_bound.octave += 1
+    upper_bound.octave += 1
+
+    midi_file = converter.parse(midi_path)
     notes_track = stream.Part()
     chords_track = stream.Part()
     key = midi_file.analyze('key')
@@ -59,15 +71,13 @@ def process_single_midi_file(midi_path, desired_duration, export_key_name, expor
 
     for element in midi_file.flatten():
         if isinstance(element, note.Note) and export_notes:
-            new_note = note.Note()
-            new_note.pitch = element.pitch
-            new_note.duration.quarterLength = desired_duration
-            notes_track.append(new_note)
+            adjusted_note = adjust_note_octave(element, lower_bound, upper_bound)
+            notes_track.append(adjusted_note)
         elif isinstance(element, chord.Chord) and export_chords:
-            new_chord = chord.Chord()
-            new_chord.pitches = element.pitches
-            new_chord.duration.quarterLength = desired_duration
-            chords_track.append(new_chord)
+            # Asegurarse de que todas las notas del acorde estén dentro del rango ajustado
+            adjusted_notes = [adjust_note_octave(n, lower_bound, upper_bound) for n in element.notes]
+            adjusted_chord = chord.Chord(adjusted_notes)
+            chords_track.append(adjusted_chord)
 
     base_filename = os.path.splitext(os.path.basename(midi_path))[0]
     if export_notes:
@@ -76,6 +86,7 @@ def process_single_midi_file(midi_path, desired_duration, export_key_name, expor
     if export_chords:
         chords_output_path = os.path.join(chords_dir, f'{base_filename}_chords{key_str}.mid')
         chords_track.write('midi', chords_output_path)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
